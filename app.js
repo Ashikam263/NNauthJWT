969346f3d1c37e1578182cd5aaaa1db17f1a40e5
Ashikam263 <ashikam263@gmail.com>
2024-05-01 10:05:13 +0530

    middlewares

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const app = express();
+const { requireAuth } = require('./middleware/authMiddleware');
 
 // middleware
 app.use(express.static('public'));
@@ -19,19 +20,19 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
-//cookies
-app.get('/set-cookies', (req, res) => {
-  // res.setHeader('Set-Cookie', 'newUser=true');
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
-  res.send('you got the cookies!');
-});
+// cookies
+// app.get('/set-cookies', (req, res) => {
+//   res.setHeader('Set-Cookie', 'newUser=true');
+//   res.cookie('newUser', false);
+//   res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
+//   res.send('you got the cookies!');
+// });
 
-app.get('/read-cookies', (req, res) => {
-  const cookies = req.cookies;
-  console.log(cookies.newUser);
-  res.json(cookies);
-});
+// app.get('/read-cookies', (req, res) => {
+//   const cookies = req.cookies;
+//   console.log(cookies.newUser);
+//   res.json(cookies);
+// });
